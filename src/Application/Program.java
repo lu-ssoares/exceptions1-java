fe9574e96a1f07576802c869debc3636672bb5d9
Lucas Soares <lu.ssoare@gmail.com>
2020-05-05 19:47:23 -0300

    Bad solution

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -21,7 +21,7 @@ public class Program {
 		Date checkOut = sdf.parse(sc.next());
 		
 		if(!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation. Check-out date must ber after check-in date");
+			System.out.println("Error in reservation. Check-out date must be after check-in date");
 		}
 		else {
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
@@ -34,20 +34,17 @@ public class Program {
 			System.out.print("Check-out: ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Eror in reservation. Dates for reservation must be future");
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error != null) {
+				System.out.println("Error in reservation: " + error); 
 			}
-			else if(!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation. Check-out date must ber after check-in date");
-			}
-			else {			
-			reservation.updateDates(checkIn, checkOut);
+			else {
 			System.out.println("Reservation: " + reservation);
+		}
 		}
 		
 		sc.close();
 	}
+	
+}
 
-}
-}
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -33,9 +33,19 @@ public class Reservation {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		
+		if(checkIn.before(now) || checkOut.before(now)) {
+			return "Eror in reservation. Dates for reservation must be future";
+		}
+		if(!checkOut.after(checkIn)) {
+			return "Error in reservation. Check-out date must ber after check-in date";
+		}		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	@Override
 	public String toString(){
